3687c32ab7efb7262396600783f166c007e4c9db
Chuli <tomasantunez.dev@gmail.com>
2025-08-26 11:26:17 -0300

    feat: add fonts to project

---
+++ src/theme/types/font.ts
@@ -0,0 +1,6 @@
+export enum Font {
+  MONTSERRAT_REGULAR = 'Montserrat-Regular',
+  MONTSERRAT_MEDIUM = 'Montserrat-Medium',
+  MONTSERRAT_SEMIBOLD = 'Montserrat-SemiBold',
+  MONTSERRAT_BOLD = 'Montserrat-Bold',
+}
